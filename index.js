ef015c6ab3c16b6000d09e6bd723bc5a02dabcc4
fabriciorby <fabriciorby@hotmail.com>
2023-02-11 11:44:49 +0100

    for tato

--- index.js
+++ index.js
@@ -2,56 +2,79 @@
 const { App } = require("@slack/bolt");
 
 const app = new App({
-    token: "xoxb-4809256745328-4771527023447-vxD3PzIIa4RxbdnswHqBB7Wq",
+    token: "",
     signingSecret: "e2b4ae6957d5ae66bc085544bae05bd5"
-  });
+});
 
-// Find conversation ID using the conversations.list method
-async function findConversation(name) {
-    try {
-      // Call the conversations.list method using the built-in WebClient
-      const result = await app.client.conversations.list();
-  
-      for (const channel of result.channels) {
-        if (channel.name === name) {
-          conversationId = channel.id;
-  
-          // Print result
-          console.log("Found conversation ID: " + conversationId);
-          // Break from for loop
-            await fetchMessage(conversationId);
-          break;
-        }
-      }
-    }
-    catch (error) {
-      console.error(error);
-    }
-  }
-
-// Fetch conversation history using the ID and a TS from the last example
-async function fetchMessage(id, ts) {
-    try {
-        const result = await app.client.conversations.history({
-            channel: id
-          });
-        
-          conversationHistory = result.messages
-            .filter(message => message.subtype != "channel_join")
-            .map(message => ({
-                user: message.user,
-                text: message.text,
-                timestamp: message.ts
-            }))
-
-        // Print results
-        console.log(conversationHistory.length + " messages found in " + id);
-        console.log(conversationHistory);
-    }
-    catch (error) {
-        console.error(error);
-    }
+async function main() {
+    publicChannelIds = (await findAllPublicChannels())
+        .map(a => a.id);
+    await enterChannels(publicChannelIds);
+    await getMessagesFromChannels(publicChannelIds);
 }
-  // Find conversation with a specified channel `name`
-  findConversation("general");
-  //fetchMessage("C04P3DFG04T")
+
+async function enterChannels(channelIds) {
+    botId = await getBotId();
+    console.log("Bot ID: " + botId);
+    console.log("Channels IDs: " + channelIds);
+    await Promise.all(channelIds.map(channel => app.client.conversations.join({ channel: channel})))
+}
+
+async function getBotId() {
+    console.log("Getting botId...");
+    return (await app.client.auth.test()).bot_id;
+}
+
+async function findAllPublicChannels() {
+    console.log("Getting all public conversations...");
+    return (await app.client.conversations.list()).channels
+        .filter(channel => channel.is_private == false)
+        .map(channel => ({
+            "name": channel.name,
+            "id": channel.id
+        }))
+        .map(printAndReturn);
+}
+
+async function getMessagesFromChannels(channelIds) {
+    return channelIds
+        .map(id => getMessagesFromChannel(id))
+        .map(printAndReturn)
+
+}
+
+async function getMessagesFromChannel(channelId) {
+    console.log((await recurseOverChannelHistory(channelId)));
+    conversationHistory = (await recurseOverChannelHistory(channelId)).messages
+        .filter(message => message.subtype != "channel_join")
+        .map(message => ({
+            user: message.user,
+            text: message.text,
+            timestamp: message.ts
+        }))
+    console.log(conversationHistory);
+}
+
+async function recurseOverChannelHistory(channelId, state = { result: [] }) {
+    if (state.lastMessageTimestamp) {
+        apiCallOptions.latest = state.lastMessageTimestamp;
+    }
+    const response = await app.client.conversations.history({ channel: channelId });
+    state.result = state.result.concat(response.messages);
+    state.lastMessageTimestamp = state.result[state.result.length - 1].ts;
+    if (response.has_more) {
+        return recurseOverChannelHistory(channelId, state);
+    }
+    return state.result.reverse();
+}
+
+function printAndReturn(obj) {
+    console.log(obj)
+    return obj;
+}
+// Find conversation with a specified channel `name`
+//findConversation("general");
+//fetchMessage("C04P3DFG04T");
+//findAllConversations();
+//enterAllPublicChannels();
+main();
